4c6268f4b23e4effad4ac66936094c3ed6a70202
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-09-10 16:57:26 -0500

    Fix: local env-script get var from .env.local file

--- src/config/ipfs/index.js
+++ src/config/ipfs/index.js
@@ -16,24 +16,16 @@ export const ipfs = ipfsClient.create({
 })
 
 if (process.env.REACT_APP_DEBUG === '1') {
-    console.log('>>--> process.env.REACT_APP_IPFS_PROJECT_ID', process.env.REACT_APP_IPFS_PROJECT_ID)
-    console.log('>>--> process.env.REACT_APP_IPFS_PROJECT_SECRET', process.env.REACT_APP_IPFS_PROJECT_SECRET)
-    console.log(
-        '>>--> REACT_APP_IPFS_HOST',
-        process.env.REACT_APP_IPFS_HOST
-    )
-    console.log(
-        '>>--> REACT_APP_IPFS_PORT',
-        process.env.REACT_APP_IPFS_PORT
-    )
-    console.log(
-        '>>--> REACT_APP_IPFS_PROTOCOL',
-        process.env.REACT_APP_IPFS_PROTOCOL
-    )
-    console.log(
-        '>>--> REACT_APP_IPFS_PUBLIC_URL',
-        process.env.REACT_APP_IPFS_PUBLIC_URL
-    )
+    console.log('>>--> REACT_APP_SC_CONVERTER_GOERLI', process.env.REACT_APP_SC_CONVERTER_GOERLI)
+    console.log('>>--> REACT_APP_SC_CONVERTER_ETHEREUM', process.env.REACT_APP_SC_CONVERTER_ETHEREUM)
+    console.log('>>--> REACT_APP_SC_MINE_GOERLI', process.env.REACT_APP_SC_MINE_GOERLI)
+    console.log('>>--> REACT_APP_SC_MINE_ETHEREUM', process.env.REACT_APP_SC_MINE_ETHEREUM)
+    console.log('>>--> REACT_APP_IPFS_PROJECT_ID', process.env.REACT_APP_IPFS_PROJECT_ID)
+    console.log('>>--> REACT_APP_IPFS_PROJECT_SECRET', process.env.REACT_APP_IPFS_PROJECT_SECRET)
+    console.log('>>--> REACT_APP_IPFS_HOST', process.env.REACT_APP_IPFS_HOST)
+    console.log('>>--> REACT_APP_IPFS_PORT', process.env.REACT_APP_IPFS_PORT)
+    console.log('>>--> REACT_APP_IPFS_PROTOCOL', process.env.REACT_APP_IPFS_PROTOCOL)
+    console.log('>>--> REACT_APP_IPFS_PUBLIC_URL', process.env.REACT_APP_IPFS_PUBLIC_URL)
 }
 
 export const ipfsPublicURL = process.env.REACT_APP_IPFS_PUBLIC_URL
